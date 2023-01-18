d6eca7c194223fb20405758a1b7e18e7efb0a268
CW1224 <xixiwang1224@gmail.com>
2023-01-18 21:48:21 +0000

    Tested to confirm the link between the css sheet and the main index

---
+++ assets/js/bookings.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Enjoy a taste of the world's amazing dishes">
+    <meta name="keyword" content="chef booking dish amazing">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Amazing Dishes at Worldwide Cuisine</title>
+</head>
+<body>
+    <section>
+        <header>
+        </header>
+    </section>
+    <section>
+        
+    </section>
+    <section>
+        
+    </section>
+    <section>
+        
+    </section>
+</body>
